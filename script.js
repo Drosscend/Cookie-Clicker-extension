13df2b37181f93f30e3a24f1c1911d783f95a77e
Drosscend <47970260+Drosscend@users.noreply.github.com>
2022-10-12 18:32:53 +0200

    fix : var to let

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 setInterval(() => {
-  var elements = document.getElementsByClassName("shimmer");
-  for (var i = 0; i < elements.length; i++) {
+  let elements = document.getElementsByClassName("shimmer");
+  for (let i = 0; i < elements.length; i++) {
     elements[i].click();
   }
 });
