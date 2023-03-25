0756b680c3395777e1153ac5f6dbffa6a8a5cd41
Drosscend <47970260+Drosscend@users.noreply.github.com>
2023-03-25 18:36:26 +0100

    feat : Ajout du click sur le cookie

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-setInterval(() => {
+function clickShimmer() {
   let elements = document.getElementsByClassName("shimmer");
   for (let i = 0; i < elements.length; i++) {
     elements[i].click();
   }
-});
+}
+
+// Définit une fonction pour cliquer sur le bouton bigCookie
+function clickBigCookie() {
+  let bigCookieButton = document.getElementById('bigCookie');
+  if (bigCookieButton) {
+    bigCookieButton.click();
+  }
+}
+
+function main(){
+  clickBigCookie();
+  clickShimmer();
+}
+
+setInterval(main);
